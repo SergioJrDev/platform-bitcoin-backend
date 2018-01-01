640e0b8f3fcc95e6ac1f0863abec089fc6212b79
Sergio Jr <sergioamjr91@gmail.com>
2018-01-01 14:15:47 -0200

    Filtrando por usuário

--- src/api/throw/throwService.js
+++ src/api/throw/throwService.js
@@ -3,4 +3,12 @@ const throwSchema = require('./throwSchema')
 throwSchema.methods(['get', 'post', 'put', 'delete'])
 throwSchema.updateOptions({new: true, runValidators: true})
 
+throwSchema.route('search', (req, res, next) => {
+    throwSchema.find({userId: req.query.user}, (err, data) => {
+        err ? console.log('error') : res.json(data);
+    })
+})
+
+// localhost:3003/api/throw/search?user=41149403837
+
 module.exports = throwSchema
