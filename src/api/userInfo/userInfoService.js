176cb45acdc66528e377fe478c7396edd865aef5
Sergio Jr <sergioamjr91@gmail.com>
2018-01-06 20:15:44 -0200

    Pesquisando usuario

--- src/api/userInfo/userInfoService.js
+++ src/api/userInfo/userInfoService.js
@@ -3,4 +3,11 @@ const userInfoSchema = require('./userInfoSchema')
 userInfoSchema.methods(['get', 'post', 'put', 'delete'])
 userInfoSchema.updateOptions({new: true, runValidators: true})
 
+userInfoSchema.route('search', (req, res, next) => {
+  const query = req.query.user || ''
+  userInfoSchema.find({nome: {$regex : ".*"+ query +".*", $options: 'i'}}, (err, data) => {
+    err ? console.log('error') : res.json(data)
+  })
+})
+
 module.exports = userInfoSchema
