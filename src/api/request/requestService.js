dcd8acbbf26e4d355bd828a5d66f05b2529f3226
Sergio Jr <sergioamjr91@gmail.com>
2018-01-08 10:59:06 -0200

    Buscando solicitaçoes

--- src/api/request/requestService.js
+++ src/api/request/requestService.js
@@ -9,4 +9,10 @@ requestSchema.route('count', (req, res, next) => {
     })
 })
 
+requestSchema.route('search', (req, res, next) => {
+  requestSchema.find({userId: req.query.user}, (err, data) => {
+      err ? console.log('error') : res.json(data);
+  })
+})
+
 module.exports = requestSchema
