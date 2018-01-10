bfaa7de461c5124df427e18a2cf8c63e1a2b1936
Sérgio Junior <sergioamjr91@gmail.com>
2018-01-10 10:16:06 -0200

    Adicionado console

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,11 @@ const mongoose = require('mongoose');
 const url = process.env.MONGOLAB_URI || 'mongodb://localhost/platform'
 mongoose.Promise = global.Promise
 
-module.exports = mongoose.connect(url, { userMongoClient: true })
+module.exports = mongoose.connect(url, {}, function(err) {
+    if(err) {
+        console.log('ERRO DATABASE >>>>>>>>', err)
+    }
+})
 
 mongoose.Error.messages.general.required = "O campo '{PATH}' é obrigatório."
 mongoose.Error.messages.Number.min = "O valor mínimo do campo '{PATH}' é '{MIN}'."
