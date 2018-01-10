1000c3ad0b8aa3d2e2b9e1002e29b67f921cce3f
Sérgio Junior <sergioamjr91@gmail.com>
2018-01-10 10:21:32 -0200

    Update port

--- src/config/server.js
+++ src/config/server.js
@@ -3,14 +3,14 @@ var
   server = express(),
   allowCors = require('./cors')
   bodyParser = require('body-parser'),
-  port = 3003;
+  port = 80;
 
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
 server.use(allowCors)
 
-server.listen(port, function() {
-  console.log(`Backend is ruinning in port ${port}`)
+server.listen(process.env.PORT || 3003, function() {
+  console.log(`Backend is ruinning in port ${process.env.PORT || 80}`)
 });
 
 module.exports = server
